915bba6bdab7b70b98c6e24f6818407b8562c85b
Aneeq Hashmi <ahashmi@folio3.com>
2018-03-06 17:47:33 +0500

    change build commands

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,3 @@
 export const environment = {
-  production: true,
-  firebaseConfig: {
-    apiKey: "AIzaSyBZWRZbnv7J6evozXPfY7j89zZoyaD-CUU",
-    authDomain: "content-spatial-1513935922342.firebaseio.com",
-    databaseURL: "https://content-spatial-1513935922342.firebaseio.com",
-    storageBucket: "content-spatial--1513935922342.appspot.com",
-    messagingSenderId: "912425775126"
-  }
+  production: true  
 };
